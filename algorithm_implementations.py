fb0221a9b368d1fe07667f1a1ff673b701f2038c
Heidi Negrete <heidi@negrete.dev>
2023-07-08 14:42:06 -0400

    add python algorithms practice

---
+++ algorithm_implementations.py
@@ -0,0 +1,15 @@
+# Linear Search O(N)
+def linear_search(my_array, target):
+    for item in my_array:
+        if item == target:
+            return item
+
+
+# Binary Search O(Log N)
+def binary_search(my_array, target):
+    pass
+
+
+# Bubble Sort
+def bubble_sort(my_array):
+    pass
